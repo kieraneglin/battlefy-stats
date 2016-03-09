b24c97e1b435bf411dc1d6f03c7eda859549d7b3
Kieran Eglin <kieran.eglin@gmail.com>
2016-03-08 18:44:19 -0800

    Added simple routing to index

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
-var app = require("./app/server/modules/routes");
+var express = require('express');
+var app = express();
 
+// Only serve index for now.  Routing will be handled with angular
+app.get('/', function(request, response) {
+  response.sendFile(__dirname + '/public/index.html');
+});
 
-// Start the server
 var server = app.listen(3000, function() {
  console.log('Listening on port %d', server.address().port);
 });
--- app/server/modules/routes.js
+++ app/server/modules/routes.js
@@ -1,1 +1,15 @@
-// 
+module.exports = function (app) {
+
+
+    app.get('/', function (req, res) {
+
+    });
+
+    app.post('/', function (req, res) {
+
+    });
+
+    app.get('*', function (req, res) {
+        res.sendFile(__dirname + '/public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+    });
+};
