a8febb12359e05e28a4ed48df2ba22b1d03631dc
Kieran Eglin <kieran.eglin@gmail.com>
2016-03-10 16:00:35 -0800

    Initial setup for search API

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 var express = require('express');
 var app = express();
+var request = require('request');
+var config = require('./env.json');
 
-// Only serve index for now.  Routing will be handled with angular
 app.use(express.static('public'));
+
+app.get('/api/get_summoner_id/:name', function(req, res){
+
+  //https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/RiotSchmick?api_key=<key>
+  var BASEURL = 'https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/';
+  var summonerName = req.query.name;
+
+  console.log(config.api_key);
+  // request(baseUrl, function (error, response, body) {
+  //   if (!error && response.statusCode == 200) {
+  //     res.json(body); // Show the HTML for the Google homepage.
+  //   }
+  // });
+});
 
 // Angular HTML5 routing won't support reloads unless you send all routes
 // to your server's entrypoint
